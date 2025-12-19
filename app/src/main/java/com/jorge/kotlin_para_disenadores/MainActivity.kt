a42af7387206924b36e5137caeced8e4d986ec8b
Raul Catalinas <raulcatalinas@gmail.com>
2025-12-19 23:10:19 +0100

    chore: archivo MainActivity.kt actualizado

--- app/src/main/java/com/jorge/kotlin_para_disenadores/MainActivity.kt
+++ app/src/main/java/com/jorge/kotlin_para_disenadores/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // Inicializar todos los elementos como "inactivos"
         actualizarEstadoVistas(
             inactivas = listOf(
                 binding.mainActivity,
